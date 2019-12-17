d996f1814d2dcce75d3920aeea4ed110bc5bd722
Nasterys <aa.hideez@gmail.com>
2019-12-17 15:34:09 +0200

    check url method

--- src/main/java/parentPage/ParentPage.java
+++ src/main/java/parentPage/ParentPage.java
@@ -1,6 +1,8 @@
 package parentPage;
 
 import libs.ActionsWithWebElements;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -10,12 +12,14 @@ public class ParentPage {
     protected WebDriver webDriver;
     protected ActionsWithWebElements actionsWithWebElements;
     Logger logger = Logger.getLogger(getClass());
-    String baseUrl = "https://my.hideez.com/";
+    public ConfigProperties configProperties = ConfigFactory.create(ConfigProperties.class);
+    String baseUrl;
     String expectedUrl;
 
 
     public ParentPage(WebDriver webDriver, String partUrl) {
         this.webDriver = webDriver;
+        baseUrl = configProperties.base_url();
         PageFactory.initElements(webDriver,this);
         actionsWithWebElements = new ActionsWithWebElements(webDriver);
         expectedUrl = baseUrl + partUrl;
